eaea4c1e9321bfac89a5ec62c3f05cf3846446d4
hehenischal <nischallc56@gmail.com>
2025-12-01 17:59:29 +0545

    (new):added custom login page using authenticationform

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path
 from .views import * 
 urlpatterns = [
-    path('',                            index,  name="home"),
-    path('create/',                     create, name="create_article"),
-    path('detail/<slug:slug>/',         detail, name="article_detail"),
-    path('update/<int:id>/',             update, name="update_article"),
-    path('delete/<int:id>/',             delete, name="delete_article")
+    path('',                            index,      name="home"),
+    path('create/',                     create,     name="create_article"),
+    path('detail/<slug:slug>/',         detail,     name="article_detail"),
+    path('update/<int:id>/',            update,     name="update_article"),
+    path('delete/<int:id>/',            delete,     name="delete_article"),
+    path('accounts/login/',              login_view, name="login")
 
 ]
